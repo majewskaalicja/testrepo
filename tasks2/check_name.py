ba189812ba5829491d28aaa785cab77aeeb5b8bd
Daniel Majewski <meroz@silverbook.local>
2018-11-12 17:01:30 +0100

    added new tasks

---
+++ tasks2/check_name.py
@@ -0,0 +1,12 @@
+'''
+    write function that ask about name, eye color, last name,
+    also ask if somebody has an animal, if yes ask about the name
+    if no then skip question. 
+    At the end print all data like: 
+    Name: Alicja
+    Lastname: Majewska
+    Eye color: brown
+    ... etc
+    Create function that returns all the values in dictionary, 
+    so you can use it in different file
+'''
