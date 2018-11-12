6f8119d7c03e580e0d2c7bf1be99c5ee935dff9c
Unknown <alicja.cechel@gmail.com>
2018-11-12 20:41:26 +0100

    Ask about personal data

--- tasks2/check_name.py
+++ tasks2/check_name.py
@@ -2,11 +2,37 @@
     write function that ask about name, eye color, last name,
     also ask if somebody has an animal, if yes ask about the name
     if no then skip question. 
-    At the end print all data like: 
-    Name: Alicja
-    Lastname: Majewska
-    Eye color: brown
-    ... etc
+
     Create function that returns all the values in dictionary, 
     so you can use it in different file
 '''
+def check_name():
+    print('What is your name?')
+    name = input()
+
+    print('What is your last name?')
+    last_name = input()
+
+    print('How is your eye color?')
+    color = input()
+
+    print('Do you have animal?')
+    animals = input()
+
+    if animals == 'Yes':
+        print('What is name your animal?')
+        name_animal = input()
+
+    personal_data = {
+        "name": name,
+        "last_name": last_name,
+        "color": color,
+        "animals": animals,
+        "name_animal": name_animal
+    }
+
+    return personal_data
+
+pd = check_name()
+print(pd)
+
