9368bd330e27c3e0acfddd66777448859dc01062
Unknown <alicja.cechel@gmail.com>
2018-11-15 19:28:43 +0100

    login to facebook

--- automatation/start.py
+++ automatation/start.py
@@ -1,17 +1,20 @@
 import os
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 chrome_driver_path = os.path.dirname(os.path.abspath(__file__)) + '/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 
-driver.get("http://www.gazeta.pl/0,0.html")
-elem = driver.find_element_by_css_selector(".o-daily h2")
-some_text = elem.text
+driver.get("https://www.facebook.com/")
+email_input = driver.find_element_by_css_selector("#email")
+password_input = driver.find_element_by_css_selector("#pass")
+# some_text = elem.text
+email_input.send_keys("alicja.cechel@gmail.com")
+password_input.send_keys("aalicjaa241993r..")
 
-print(some_text)
+# print(some_text)
+driver.find_element_by_css_selector('[data-testid=royal_login_button]').click()
 
 driver.close()
